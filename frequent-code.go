dfde16cbe4000c0cab0dd82769025f316f3b964a
nolleh <nolleh7707@gmail.com>
2020-01-28 22:24:06 +0900

    first commit

---
+++ frequent-code.go
@@ -0,0 +1,30 @@
+package leetcode
+
+import (
+	"strconv"
+)
+
+func debug(desc string, datas ...[]int) {
+	str := ""
+	for i, data := range datas {
+		str += "data[" + strconv.Itoa(i) + "]("
+		for j, v := range data {
+			str += strconv.Itoa(v)
+			if j != len(data)-1 {
+				str += ","
+			}
+		}
+		str += ") "
+	}
+	panic(str + ":" + desc)
+}
+
+func debugstr(desc string, datas ...string) {
+	str := ""
+	for i, data := range datas {
+		str += "data[" + strconv.Itoa(i) + "]("
+		str += data
+		str += ") "
+	}
+	panic(str + ":" + desc)
+}
