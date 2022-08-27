ec2f2934c0ba64d5167761e276bc5bae96df9902
nolleh7707 <nolleh7707@gmail.com>
2022-08-27 12:24:25 +0900

    chore: solutions

--- probs/binary-tree/116.Populating-Next-Right-Ptr.py
+++ probs/binary-tree/116.Populating-Next-Right-Ptr.py
@@ -35,4 +35,70 @@ class Solution:
       if node.right:
         self.track(node.right, node, level+1, True)
     
-      
+## ---------  
+## LEVEL ORDER TRAVERSAL
+## Solution
+## while (!Q.empty())
+## {
+##     size = Q.size()
+##     for i in range 0..size
+##     {
+##         node = Q.pop()
+##         Q.push(node.left)
+##         Q.push(node.right)
+##     }
+## }
+## 
+## 
+##
+# 1. Makes Queue. (Tuple, NULL, nested loop)
+# 2. adding root of the tree in the queue (1) -> (2,3)/ 1 was poped
+# 3. first while loop iterates over each level one by one and and the inner loop over all the nodes
+# 4. (2,3) -> (3, 4, 5). also, the element at the head of the queue is the `next element` in order.
+# 5. (3, 4, 5) -> (4, 5, 6, 7)
+###
+"""
+import collections 
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        
+        if not root:
+            return root
+        
+        # Initialize a queue data structure which contains
+        # just the root of the tree
+        Q = collections.deque([root])
+        
+        # Outer while loop which iterates over 
+        # each level
+        while Q:
+            
+            # Note the size of the queue
+            size = len(Q)
+            
+            # Iterate over all the nodes on the current level
+            for i in range(size):
+                
+                # Pop a node from the front of the queue
+                node = Q.popleft()
+                
+                # This check is important. We don't want to
+                # establish any wrong connections. The queue will
+                # contain nodes from 2 levels at most at any
+                # point in time. This check ensures we only 
+                # don't establish next pointers beyond the end
+                # of a level
+                if i < size - 1:
+                  node.next = Q[0]
+                
+                # Add the children, if any, to the back of
+                # the queue
+                if node.left:
+                    Q.append(node.left)
+                if node.right:
+                    Q.append(node.right)
+        
+        # Since the tree has now been modified, return the root node
+        return root
+"""      
