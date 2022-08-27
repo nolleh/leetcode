c301aa9a5a62bf6dda59b90ca428af6e8b2b451e
nolleh7707 <nolleh7707@gmail.com>
2022-08-28 01:27:00 +0900

    feat: 53.max-subarray

---
+++ datastructure/53.maximum-subarray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        # get max! - O(n)        
+        max_sub = nums[0]
+        prv = nums[0]
+        for n in nums[1:]:
+            current = max(prv + n, n) 
+            max_sub = max(max_sub, current)
+            prv = current
+        return max_sub
+        
