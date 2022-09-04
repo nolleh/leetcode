79aa621d82d0675e1bf8e02dc21b146e6219433d
nolleh7707 <nolleh7707@gmail.com>
2022-09-04 13:38:50 +0900

    feat: time to sell

---
+++ leetcode75/level1/121.best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # maximize profit by choosing a single day to buy one stock, and choosing different day in the future to sell the stock.
+        # reutrn maximum profit. cannot achieve any profit, return 0.
+       
+        if len(prices) == 0:
+            return 0
+        if len(prices) == 1:
+            return 0
+        
+        maximum = 0
+        minimum = prices[0]
+        for p in prices:
+            maximum = max(maximum, p - minimum) 
+            minimum = min(minimum, p)
+        return maximum
+        
