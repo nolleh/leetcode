2fcce45f3348e8502c4e93f429922229e9b4f20a
nolleh7707 <nolleh7707@gmail.com>
2022-09-17 09:02:32 +0900

    feat: trie/208.implement trie

---
+++ probs/trie/208.implement-trie.py
@@ -0,0 +1,40 @@
+class Node:
+    def __init__(self):
+        self.childs = {}
+        self.finished = False
+class Trie:
+    def __init__(self):
+        self.n = Node()
+
+    def insert(self, word: str) -> None:
+        node = self.n
+        for c in word:
+            if c not in node.childs:
+                node.childs[c] = Node()
+            node = node.childs[c]
+        node.finished = True
+        
+    def search(self, word: str) -> bool:
+        node = self.n
+        for w in word:
+            c = node.childs
+            if w not in c:
+                return False
+            node = c[w]
+        return node.finished
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        node = self.n
+        for c in prefix:
+            if c not in node.childs:
+                return False
+            node = node.childs[c]
+        return True
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
