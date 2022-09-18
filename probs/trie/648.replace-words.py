4da5ed6d6ea8af0f05916227d2ac542983821b11
nolleh7707 <nolleh7707@gmail.com>
2022-09-18 09:41:43 +0900

    feat: trie/648.replace-words

---
+++ probs/trie/648.replace-words.py
@@ -0,0 +1,33 @@
+class TrieNode:
+    def __init__(self):
+        self.childs = {}
+        self.match_dict = False
+        self.leaf = False
+        
+class Solution:
+    def __init__(self):
+        self.trie = TrieNode()
+        self.dicts = {}
+    def insert(self, word: str) -> str:
+        node = self.trie
+        s = '';
+        for w in word:
+            if w not in node.childs:
+                node.childs[w] = TrieNode()
+            if s in self.dicts:
+                self.match_dict = True
+                break
+            node = node.childs[w]
+            s += w
+        node.leaf = True
+        return s
+         
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        for d in dictionary:
+            self.dicts.setdefault(d, True)
+        
+        ans = []
+        for s in sentence.split(' '):
+            ns = self.insert(s)
+            ans += [ns]
+        return ' '.join(ans) 
