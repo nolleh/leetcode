2de37a6e82dfdc077cd5de99969f93d5297f6989
nolleh7707 <nolleh7707@gmail.com>
2022-09-18 14:17:06 +0900

    feat: trie/211.design-and-search-words-data-structor.py

---
+++ probs/trie/211.design-add-and-search-words-data-structure.py
@@ -0,0 +1,31 @@
+class WordDictionary:
+    def __init__(self):
+        self.trie = {} 
+
+    def addWord(self, word: str) -> None:
+        node = self.trie
+        for w in word:
+            if not w in node:
+                node[w] = {}
+            node = node[w]
+        node['$'] = {}
+        
+    def search(self, word: str) -> bool:
+        def search_in_node(word: str, node):
+            for i, w in enumerate(word):
+                if not w in node:
+                    if w == '.':
+                        for c in node:
+                            if search_in_node(word[i + 1:], node[c]):
+                                return True
+                    return False
+                else:
+                    node = node[w]
+            return '$' in node
+        
+        return search_in_node(word, self.trie)
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
