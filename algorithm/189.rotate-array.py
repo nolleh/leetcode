63b804a7d4fab7ac966149653335a1fb296299ce
nolleh7707 <nolleh7707@gmail.com>
2022-10-09 19:46:52 +0900

    feat: 189.rotate-array

---
+++ algorithm/189.rotate-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        prefix_i = len(nums) - k % len(nums)
+        prefix = nums[prefix_i:]
+        r = len(nums) -1
+        for i in range(prefix_i -1, -1, -1):
+            nums[r] = nums[i]
+            r-=1
+        for i in range(len(prefix)):
+            nums[i] = prefix[i]
+
+        return nums
