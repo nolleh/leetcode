1a70e23cfd7a8f76dae7d084cede0dd32536a083
nolleh7707 <nolleh7707@gmail.com>
2022-10-11 23:04:41 +0900

    feat: 557.reverse-words-in-a-string-III

---
+++ algorithm/557.reverse-words-in-a-string-III.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        # reverse string while still preserving whitespace and initial word order
+        res = []
+        ws = s.split(' ')
+
+        # too verbose to implement reverse
+        # (I've already solved O(N) reverse questions 1min ago), so use builtin method
+        for w in ws:
+            res += [w[::-1]]
+
+        return " ".join(res)
