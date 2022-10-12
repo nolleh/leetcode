4e8e6852e24ef077531709fc5891e50924a22c50
nolleh7707 <nolleh7707@gmail.com>
2022-10-12 22:58:10 +0900

    feat: 878.middle-of-the-linked-list

---
+++ algorithm/876.middle-of-the-linked-list.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node = head
+        fast = head
+        while fast:
+            if not fast.next:
+                break
+            node = node.next
+            fast = fast.next.next
+        return node 
+         
