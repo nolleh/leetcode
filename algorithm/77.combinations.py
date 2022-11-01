2503c29cc536a0d04a73b2b070510d483ec35323
nolleh7707 <nolleh7707@gmail.com>
2022-11-01 12:27:05 +0900

    77.combinations

---
+++ algorithm/77.combinations.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        def backtrack(first = 1, curr = []):
+            if len(curr) == k:
+                ## append: mutate the old list. (inplace)
+                output.append(curr[:])
+                ## plus: does not mutate.
+                # output += curr[:]
+
+            for i in range(first, n + 1):
+                curr += [i]
+                # repeate for remain values
+                backtrack(i + 1, curr)
+                # remove last value for backtrack
+                curr.pop()
+            
+        output = []
+        backtrack()
+        return output
