f3e3742c3929752338fb8054deef942cf9fc5ecf
nolleh7707 <nolleh7707@gmail.com>
2022-12-11 21:19:06 +0900

    702.search-in-a-sorted-array-of-unknown-size

---
+++ probs/binary-search/702.search-in-a-sorted-array-of-unknown-size.py
@@ -0,0 +1,28 @@
+# """
+# This is ArrayReader's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class ArrayReader:
+#    def get(self, index: int) -> int:
+
+class Solution:
+    def search(self, reader: 'ArrayReader', target: int) -> int:
+        
+        l = 0 
+        r = 1
+        # find boundaries
+        while reader.get(r) < target:
+            l = r
+            r = r * 2
+        # previous loops termination condition: includes target == reader.get(r)
+        # so next while loop should contains r's value
+        while l <= r:
+            mid = (r - l) // 2 + l
+            if target == reader.get(mid):
+                return mid
+            elif target > reader.get(mid):
+                l = mid + 1
+            else:
+                r = mid - 1
+        return -1
+        
