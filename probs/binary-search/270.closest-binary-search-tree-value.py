c399528f3f5a710096d06b18291894a51689c9d5
nolleh7707 <nolleh7707@gmail.com>
2022-12-11 17:04:49 +0900

    280.colosest binary search tree value

---
+++ probs/binary-search/270.closest-binary-search-tree-value.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def closestValue(self, root: Optional[TreeNode], target: float) -> int:
+        # if not root:
+        #     return sys.maxsize
+        # if root.val > target:
+        #     leftres = self.closestValue(root.left, target) 
+        #     if abs(leftres - target) < abs(root.val - target):
+        #         return leftres
+        #     else:
+        #         return root.val
+        # else:
+        #     rightres = self.closestValue(root.right, target)
+        #     if abs(rightres - target) < abs(root.val - target):
+        #         return rightres
+        #     else:
+        #         return root.val
+	    def inorder(r: TreeNode):
+			return inorder(r.left) + [r.val] + inorder(r.right) if r else []	
+		return min(inorder(root, key = lambda x: abs(target - x)))
