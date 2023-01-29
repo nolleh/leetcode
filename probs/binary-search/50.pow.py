d4d9183df46b399e3c5f4d646fadeb2160c67a49
nolleh7707 <nolleh7707@gmail.com>
2023-01-30 00:14:21 +0900

    feat(binary-search): pow

---
+++ probs/binary-search/50.pow.py
@@ -0,0 +1,14 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n < 0:
+            x = 1 / x
+            n = -n
+        ans = 1
+        current_product = x
+
+        while n > 0:
+            if n % 2 == 1:
+                ans *= current_product
+            current_product *= current_product
+            n //= 2
+        return ans         
