64d7279d9f636409899118372124d2cf189ec12f
Kyeong-mi Kim <nolleh7707@gmail.com>
2025-12-20 19:13:25 +0900

    Refactor: sum adjacent gains for stock II

--- top-interview-150/122.best-time-to-buy-and-sell-stock-2.py
+++ top-interview-150/122.best-time-to-buy-and-sell-stock-2.py
@@ -1,15 +1,9 @@
+from typing import List
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # 'YOU CAN ONLY HOLD AT MOST ONE'
-
-        cost = -1
         profit = 0
-        prices.append(0)
-        for i in range(len(prices) - 1):
-            if prices[i] > prices[i + 1]:
-                if cost != -1 and prices[i]:
-                    profit += prices[i] - cost
-                    cost = -1
-            elif prices[i] < prices[i + 1] and cost == -1:
-                cost = prices[i]
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i - 1]
         return profit
